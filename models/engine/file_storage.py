0ce1025d06e376fb22ca35177ea128372f5ff591
ahmedhassan726796 <ahmedhassan726796>
2024-01-14 12:24:18 -0800

    adding enginge

---
+++ models/engine/file_storage.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""FileStorage class"""
+import json
+import os
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """define a FileStorage class"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary of __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        if obj:
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file(path:__file_path)"""
+        Dict = {}
+        for key, value in self.__objects.items():
+            Dict[key] = value.to_dict()
+
+        with open(self.__file_path, "w") as f:
+            json.dump(Dict, f)
+
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        if not os.path.exists(self.__file_path):
+            return
+
+        with open(self.__file_path, 'r') as f:
+            objs = json.load(f)
+            self.__objects = {}
+            for key, value in objs.items():
+                class_name = value["__class__"]
+                self.__objects[key] = eval(class_name)(**objs[key])
